4bf248f476e8ee5bf365db2cf3bf7a71103766b5
CO1090 <119437960+CO1090@users.noreply.github.com>
2023-08-04 02:38:52 +0100

    adding animation to html using js

--- javascript/index.js
+++ javascript/index.js
@@ -38,3 +38,20 @@ window.onscroll = () => {
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
+
+//scroll reveal
+
+document.addEventListener("DOMContentLoaded", function () {
+  ScrollReveal({
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+  });
+
+  ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+  ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+  ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+  ScrollReveal().reveal(".home-img, .portfolio-box, .contact form", {
+    origin: "bottom",
+  });
+});
